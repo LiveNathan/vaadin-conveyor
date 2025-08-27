aa0e7efbedf113826145436dd88ee44606d40e3c
start.vaadin.com <no-reply@vaadin.com>
2025-08-27 12:58:54 +0000

    Generated project

---
+++ src/main/java/dev/nathanlively/Application.java
@@ -0,0 +1,24 @@
+package dev.nathanlively;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@PageTitle("Vaadin+Conveyor POC")
+@Theme(value = "vaadin-conveyor")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
